c2321fcdca8a9cd5c5bf8e5689d8f5fdd77999f5
GustaveGaban <107839859+GustaveGaban@users.noreply.github.com>
2022-07-21 21:37:11 +0200

    2d10

--- S1/serie-01/exo-04/exo-04.js
+++ S1/serie-01/exo-04/exo-04.js
@@ -1,12 +1,13 @@
 let nombre = (Math.random()*100).toFixed(0);
 console.log(nombre)
 
-let messagehtml=document.getElementById("messagehtml");
-messagehtml.innerHTML="Nombre aléatoire entre 0 et 100 : "+nombre;
+document.getElementById("messagehtml").innerHTML="Nombre aléatoire entre 0 et 100 : "+nombre;
+
 
 let nbre1_1d10=(Math.random()*9).toFixed(0);
 let nbre2_1d10=(Math.random()*9).toFixed(0);
 console.log(nbre1_1d10, nbre2_1d10);
-let d10system=document.getElementById("d10system");
-d10system.innerHTML="Système 100 avec 2D10 : "+nbre1_1d10+" et "+nbre2_1d10
+
+document.getElementById("d10system").innerHTML="Système 2D10 : "+nbre1_1d10+" et "+nbre2_1d10;
+
 
